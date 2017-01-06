036e0b17fc29a8aa16bc134ea8d8513649d8ebdc
Coolteenz <hugo.sacchi@gmail.com>
2017-01-06 11:53:24 +0100

    Player Move, background, few platforms

---
+++ Assets/Scripts/EnemyScript.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyScript : MonoBehaviour {
+
+    [SerializeField] Transform playerTransform;
+    Rigidbody2D m_body;
+
+    void Awake()
+    {
+        m_body = GetComponent<Rigidbody2D>();
+    }
+
+	// Use this for initialization
+	void Start ()
+    {
+	
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        m_body.velocity = (playerTransform.position = transform.position).normalized;
+	
+	}
+}
---
+++ Assets/Scripts/PlayerControllerScript.cs
@@ -0,0 +1,57 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerControllerScript : MonoBehaviour
+{
+
+    [SerializeField]
+    float maxSpeed =10f;
+    [SerializeField]
+    bool facingRight = true;
+  
+
+    Animator anim;
+
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    private void Update()
+    {
+
+    }
+
+
+    void FixedUpdate()
+    {
+        float move = Input.GetAxis("Horizontal");
+
+        anim.SetFloat("Speed", GetComponent<Rigidbody2D>().velocity.y);
+
+
+        GetComponent<Rigidbody2D>().velocity = new Vector3(maxSpeed * move, GetComponent<Rigidbody2D>().velocity.y);
+
+
+        //Horizontal is used to start the walking animation
+        anim.SetFloat("Speed", Mathf.Abs(move));
+
+        if (move > 0 && !facingRight)
+        {
+            Flip();
+        }
+        else if (move < 0 && facingRight)
+        {
+            Flip();
+        }
+
+    }
+    void Flip()
+    {
+        facingRight = !facingRight;
+        Vector3 theScale = transform.localScale;
+        theScale.x *= -1;
+        transform.localScale = theScale;
+
+    }
+}
