293324c62d173dcf274b2457ade986f51ee5b102
Coolteenz <hugo.sacchi@gmail.com>
2017-01-14 16:06:51 +0100

    Adding Shot and Menu (not finished)

---
+++ Assets/Scripts/MenuScript.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+
+public class MenuScript : MonoBehaviour
+{
+    [SerializeField]
+    Button[] menuButtons;
+
+    public void switchScene(string sceneName)
+    {
+        SceneManager.LoadScene(sceneName);
+    }
+}
--- Assets/Scripts/PlayerControllerScript.cs
+++ Assets/Scripts/PlayerControllerScript.cs
@@ -5,10 +5,21 @@ public class PlayerControllerScript : MonoBehaviour
 {
 
     [SerializeField]
-    float maxSpeed =10f;
+    float maxSpeed = 10f;
     [SerializeField]
     bool facingRight = true;
-  
+    [SerializeField]
+    bool grounded = false;
+    [SerializeField]
+    float groundRadius = 0.2f;
+    [SerializeField]
+    Transform groundCheck;
+    [SerializeField]
+    LayerMask whatIsGround;
+    [SerializeField]
+    float jumpForce = 350f;
+
+
 
     Animator anim;
 
@@ -19,12 +30,41 @@ public class PlayerControllerScript : MonoBehaviour
 
     private void Update()
     {
+        if (grounded && Input.GetKeyDown(KeyCode.Space))
+        {
+            anim.SetBool("Ground", false);
+            GetComponent<Rigidbody2D>().AddForce(new Vector2(0, jumpForce));
+        }
+        // 5 - Shooting
+        bool shoot = Input.GetButtonDown("Fire1");
+        shoot |= Input.GetButtonDown("Fire2");
+        // Careful: For Mac users, ctrl + arrow is a bad idea
+
+        if (shoot)
+        {
+            WeaponScript weapon = GetComponent<WeaponScript>();
+            if (weapon != null)
+            {
+                // false because the player is not an enemy
+                weapon.Attack(false);
+            }
+        }
 
     }
 
 
+
     void FixedUpdate()
     {
+        grounded = Physics2D.OverlapCircle(groundCheck.position, groundRadius, whatIsGround);
+        anim.SetBool("Ground", grounded);
+
+        anim.SetFloat("vSpeed", GetComponent<Rigidbody2D>().velocity.y);
+
+
+
+
+
         float move = Input.GetAxis("Horizontal");
 
         anim.SetFloat("Speed", GetComponent<Rigidbody2D>().velocity.y);
@@ -33,7 +73,6 @@ public class PlayerControllerScript : MonoBehaviour
         GetComponent<Rigidbody2D>().velocity = new Vector3(maxSpeed * move, GetComponent<Rigidbody2D>().velocity.y);
 
 
-        //Horizontal is used to start the walking animation
         anim.SetFloat("Speed", Mathf.Abs(move));
 
         if (move > 0 && !facingRight)
@@ -44,6 +83,7 @@ public class PlayerControllerScript : MonoBehaviour
         {
             Flip();
         }
+
 
     }
     void Flip()
